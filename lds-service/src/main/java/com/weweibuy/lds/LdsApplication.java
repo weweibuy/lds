9ab4328c9e21a2be3ce556b5bad0e18721748246
durenhao <durenhao@longfor.com>
2021-09-02 18:15:06 +0800

    init

---
+++ lds-service/src/main/java/com/weweibuy/lds/LdsApplication.java
@@ -0,0 +1,19 @@
+package com.weweibuy.lds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 日志分发服务
+ *
+ * @author durenhao
+ * @date 2021/9/2 18:05
+ **/
+@SpringBootApplication
+public class LdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdsApplication.class, args);
+    }
+
+}
